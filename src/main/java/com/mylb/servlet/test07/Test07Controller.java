f61de3e911e6d07df7cf0374e911ed3922943b64
kdw <kdw@gmail.com>
2023-06-23 19:06:32 +0900

    [김도원] - test07,08 추가

--- src/main/java/com/mylb/servlet/test07/Test07Controller.java
+++ src/main/java/com/mylb/servlet/test07/Test07Controller.java
@@ -3,10 +3,12 @@ package com.mylb.servlet.test07;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/servlet/test07")
 public class Test07Controller extends HttpServlet {
 	
 	@Override
@@ -15,16 +17,24 @@ public class Test07Controller extends HttpServlet {
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/html");
 		
+		String address = request.getParameter("address");
+		String card = request.getParameter("card");
+		String money = request.getParameter("money");
+		
 		PrintWriter out = response.getWriter();
 		
-		String address = request.getParameter("address");
-		String cardName = request.getParameter("cardName");
-		int price = Integer.parseInt(request.getParameter("price"));
+		out.print("<html><head><title>배탈의민족</title></head> <body>");
 		
-		if(address.contains("서울시")) {
-			out.println("<html><head><title>주문 결과</title></head><body>");
-			out.println("<h3>배달 불가 지역입니다.</h3></body></html>");
+		if(!address.contains("서울시")) {
+			out.print("<h1>배달 불가 지역입니다.</h1>");
+		} else if(card.equals("신한카드")) {
+			out.print("<h1>결제 불가능한 카드입니다.</h1>");
+		} else {
+			out.print("<h3>" + address + "로 배달 준비중</h3> <hr>");
+			out.print("결제금액 : " + money + "원");
 		}
+		
+		out.print("</body></html>");
 	}
 
 }
